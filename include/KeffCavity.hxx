a07edbb204776f08ea0f292c109c324fcfd842d6
Simon Vidanovic <dvvidanovic@lbl.gov>
2019-02-28 10:04:13 -0800

    CMake for Windows-CalcEngine and google test download set up. Project is still empty.

---
+++ include/KeffCavity.hxx
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "../src/KeffC.hxx"
---
+++ tst/FrameCavity_ISO15099.unit.cxx
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+class TestFrameCavity_ISO15099 : public testing::Test
+{
+
+protected:
+    void SetUp() override
+    {
+        
+    }
+
+
+};
+
+TEST_F(TestFrameCavity_ISO15099, Test1)
+{
+
+}
