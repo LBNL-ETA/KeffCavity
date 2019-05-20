ad215832159ac56a6f6055a2d20eb1466957fdf5
Simon Vidanovic <dvvidanovic@lbl.gov>
2019-05-20 14:48:10 -0700

    MD flag is no longer replaced with MT.

--- cmake/KeffCinternal_utils.cmake
+++ cmake/KeffCinternal_utils.cmake
@@ -16,7 +16,7 @@
 #
 # This must be a macro(), as inside a function string() can only
 # update variables in the function scope.
-macro(fix_default_compiler_settings_)
+macro(fix_default_compiler_settings_KeffC)
   if (MSVC)
     # For MSVC, CMake sets certain flags to defaults we want to override.
     # This replacement code is taken from sample in the CMake Wiki at
@@ -24,17 +24,6 @@ macro(fix_default_compiler_settings_)
     foreach (flag_var
              CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
              CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-      if (NOT BUILD_SHARED_LIBS AND NOT gtest_force_shared_crt)
-        # When Google Test is built as a shared library, it should also use
-        # shared runtime libraries.  Otherwise, it may end up with multiple
-        # copies of runtime library data in different modules, resulting in
-        # hard-to-find crashes. When it is built as a static library, it is
-        # preferable to use CRT as static libraries, as we don't have to rely
-        # on CRT DLLs being available. CMake always defaults to using shared
-        # CRT libraries, so we override that default here.
-        string(REPLACE "/MD" "-MT" ${flag_var} "${${flag_var}}")
-      endif()
-
       # We prefer more strict warning checking for building Google Test.
       # Replaces /W3 with /W4 in defaults.
       string(REPLACE "/W3" "-W4" ${flag_var} "${${flag_var}}")
@@ -51,7 +40,7 @@ macro(config_compiler_and_linker_KeffC)
     find_package(Threads)
   endif()
 
-  fix_default_compiler_settings_()
+  fix_default_compiler_settings_KeffC()
   if (MSVC)
     # Newlines inside flags variables break CMake's NMake generator.
     # TODO(vladl@google.com): Add -RTCs and -RTCu to debug builds.
