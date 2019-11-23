3177b33e6ff11c1219693d279568d140269d1ece
Simon Vidanovic <dvvidanovic@lbl.gov>
2019-11-22 16:00:16 -0800

    Gravity vector have default values of 0, -1, 0

--- src/GravityVector.hxx
+++ src/GravityVector.hxx
@@ -3,6 +3,10 @@
 namespace FenestrationCommon {
 //! Storage for unity vector that represents gravity direction
     struct GravityVector {
+        GravityVector() : x{0}, y{-1}, z{0} {}
+
+        GravityVector(const double x, const double y, const double z) : x(x), y(y), z(z) {}
+
         double x;
         double y;
         double z;
